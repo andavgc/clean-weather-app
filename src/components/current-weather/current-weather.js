6a6e9040d3ddd30759882713adc9991dafa741ef
andav <andav_@gitlab.com>
2023-02-21 16:45:16 -0300

    first commit

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,49 @@
+import "./current-weather.css";
+
+const today = new Date();
+var month = today.getUTCMonth() + 1;
+var day = today.getUTCDate();
+const todayDate = day + "/" + month
+console.log(todayDate)
+const CurrentWeather = ({ data }) => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-description">{data.weather[0].description}</p>
+                </div>
+                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
+            </div>
+            <div className="bottom">
+                <div className="main-info">
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
+                <p className="today">{todayDate}</p>
+                </div>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label top">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">{data.main.pressure} hPa</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CurrentWeather;
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,80 @@
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
+import './forecast.css';
+
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    const today = new Date();
+
+    //Get next days list
+    let nextDate = new Date();
+
+    const forecastDates = []
+    for (let i = 1; i < 8; i++) {
+        nextDate.setDate(today.getDate() + i)
+        var day = nextDate.getUTCDate();
+        var month = nextDate.getUTCMonth() + 1;
+        forecastDates.push(day + "/" + month)
+
+    }
+
+    return (
+        <div className="forecast">
+            <label className="title">Forecast: next 7 days</label>
+            <Accordion allowZeroExpanded>
+                {data.list.slice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                    <div className="date">
+                                    <label className="week-day">{forecastDays[idx]}</label>
+                                    <label className="day-date">{forecastDates[idx]}</label>
+                                    </div>
+                                    <label className="description">{item.weather[0].description}</label>
+                                    <label className="min-max">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label>Pressure</label>
+                                    <label>{item.main.pressure} hPa</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Humidity</label>
+                                    <label>{item.main.humidity}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Clouds</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Wind speed:</label>
+                                    <label>{item.wind.speed} m/s</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Sea level:</label>
+                                    <label>{item.main.sea_level}m</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Feels like:</label>
+                                    <label>{Math.round(item.main.feels_like)}°C</label>
+                                </div>
+                            </div>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+
+                ))}
+                <AccordionItem></AccordionItem>
+            </Accordion>
+        </div>
+    )
+};
+
+export default Forecast;
