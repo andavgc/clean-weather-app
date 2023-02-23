0d74db841b0821026b6b4d56acfb5eb209b24da6
andav <andav_@gitlab.com>
2023-02-23 09:48:49 -0300

    privating

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GeoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1e69abbf2cmsh4c8046804cd4980p12d6bfjsn070ab67e8d7d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY= "9b4ed89c28d6c63f65c78c386f8dbdfc"
